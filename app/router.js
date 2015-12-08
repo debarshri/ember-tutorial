4c551dc2a581f7c96e73b5247af167392b1d1e1e
dbasak <dbasak@bol.com>
2015-12-08 12:59:50 +0100

    Upto Router tutorial

--- app/router.js
+++ app/router.js
@@ -6,9 +6,16 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('about');
+  this.route('index', { path: '/' });
   this.route('contact');
   this.route('table');
+  this.route('about', { path: '/about' });
+  this.route('favorites', { path: '/favs' });
+  this.route('posts', function() {
+    this.route('new');
+  });
+  this.route('page-not-found', { path: '/*wildcard' });
+  this.route('post', { path: '/post/:post_id' });
 });
 
 export default Router;
